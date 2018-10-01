fdd6af5933e74fc012bd7e5608690d28837cba6f
Kameron Lutes <kalutes@purdue.edu>
2018-10-01 10:46:05 -0700

    Add success message to setup script

--- TODO_6_Setup.sh
+++ TODO_6_Setup.sh
@@ -8,5 +8,20 @@ git commit -m "LOL GET REKT I JUST PERMANENTLY DELETED answers.txt"
 
 git push
 
-echo "\n\nHAHAHAHA I PERMANENTLY DELETED ALL OF YOUR PROGRESS ON THIS LAB.\nHAVE FUN SPENDING AN HOUR AND DOING THE WHOLE THING AGAIN!!!!\n\n\n"
+echo "HAHAHAHA I PERMANENTLY DELETED ALL OF YOUR PROGRESS ON THIS LAB."
+echo
+echo
+echo "HAVE FUN SPENDING AN HOUR AND DOING THE WHOLE THING AGAIN!!!!"
 
+cat << "EOF"
+
+88                                88                           88  
+88                                88                           88  
+88                                88                           88  
+88,dPPYba,  ,adPPYYba,  ,adPPYba, 88   ,d8  ,adPPYba,  ,adPPYb,88  
+88P'    "8a ""     `Y8 a8"     "" 88 ,a8"  a8P_____88 a8"    `Y88  
+88       88 ,adPPPPP88 8b         8888[    8PP""""""" 8b       88  
+88       88 88,    ,88 "8a,   ,aa 88`"Yba, "8b,   ,aa "8a,   ,d88  
+88       88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a `"Ybbd8"'  `"8bbdP"Y8
+
+EOF
